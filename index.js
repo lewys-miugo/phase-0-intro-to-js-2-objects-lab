d28656784ad9b04a5b4def1439e96b1626aeb07e
lewys-miugo <lewiswambugu01@gmail.com>
2025-06-19 09:23:27 +0300

    Writing functions to manipulate objects

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+let employee = {
+  name:"",
+  streetAddress:""
+}
+
+function updateEmployeeWithKeyAndValue(employee,key,value) {
+  return {...employee,[key]:value};
+}
+
+// destructivelyUpdateEmployeeWithKeyAndValue(): this function should work the same as updateEmployeeWithKeyAndValue() but it should mutate the employee Object passed in.
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) {
+  employee[key]=value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee,key) {
+  let newEmployee = {...employee};
+  // console.log(newEmployee);
+  // console.log(employee);
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+// deleteFromEmployeeByKey(employee, 'name');
+
+function destructivelyDeleteFromEmployeeByKey(employee,key) {
+  delete employee[key];
+  return employee;
+}
